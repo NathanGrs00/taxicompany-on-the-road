10d709492d4fab7182c9fef1ca020a56e0303275
NathanGRSDesktop <nathangeers00@gmail.com>
2025-03-07 12:49:04 +0100

    Feat: Initial homepage, labels and input fields.

---
+++ app/src/main/java/com/nathan/taxibedrijf_on_the_road/MainActivity.kt
@@ -0,0 +1,17 @@
+package com.nathan.taxibedrijf_on_the_road
+
+import android.os.Bundle
+import android.widget.TextView
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_main)
+
+    }
+}
