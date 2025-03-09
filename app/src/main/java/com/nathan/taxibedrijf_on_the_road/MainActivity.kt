8ccd3f8ca029e5fd680d1f06750b26b54be8bfd5
NathanGRSDesktop <nathangeers00@gmail.com>
2025-03-09 21:37:29 +0100

    Feat: Show user input on screen.

--- app/src/main/java/com/nathan/taxibedrijf_on_the_road/MainActivity.kt
+++ app/src/main/java/com/nathan/taxibedrijf_on_the_road/MainActivity.kt
@@ -1,17 +1,28 @@
 package com.nathan.taxibedrijf_on_the_road
 
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
 import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
 
 class MainActivity : AppCompatActivity() {
+
+    lateinit var kentekenInvoer: EditText
+    lateinit var toonGegevens: TextView
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_main)
 
+        kentekenInvoer = findViewById(R.id.kentekenInvoer)
+        toonGegevens = findViewById(R.id.autoGegevensText)
+
+        val verzendKentekenKnop = findViewById<Button>(R.id.verzendKentekenKnop)
+        verzendKentekenKnop.setOnClickListener(){
+            toonGegevens.text = kentekenInvoer.text
+        }
     }
 }
