9e4c3a07220548cb522cddbfd51ffb9d77932859
NathanGRSDesktop <nathangeers00@gmail.com>
2025-03-18 12:38:18 +0100

    Added back navigation, modularized navbar, and fixed layout adjustments
    - Added a "Go Back" button in DetailsActivity to navigate back to the main activity.
    - Modularized the navigation bar by creating a reusable layout file (`navigation_bar.xml`).
    - Adjusted the layout positioning in `details_layout.xml` to fine-tune UI placement.
    - Added a primary green color in `colors.xml` for styling the back button.

--- app/src/main/java/com/nathan/taxibedrijf_on_the_road/DetailsActivity.kt
+++ app/src/main/java/com/nathan/taxibedrijf_on_the_road/DetailsActivity.kt
@@ -1,6 +1,7 @@
 package com.nathan.taxibedrijf_on_the_road
 
 import android.os.Bundle
+import android.widget.Button
 import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 
@@ -14,5 +15,10 @@ class DetailsActivity : AppCompatActivity() {
         val tvKentekenInvoer: TextView = findViewById(R.id.tvKentekenInvoer)
 
         tvKentekenInvoer.text = bundle!!.get("KENTEKEN_INVOER").toString()
+
+        val btnTerug: Button = findViewById(R.id.btnTerug)
+        btnTerug.setOnClickListener(){
+            finish()
+        }
     }
 }
