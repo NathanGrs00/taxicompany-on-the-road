b15c780dab784bda8cff2874cbcc6c805c2d3a5b
NathanGRSDesktop <nathangeers00@gmail.com>
2025-03-19 21:00:45 +0100

    feat: Log RDW API response to console
    - Added Volley dependency for network requests.
    - Implemented a GET request to RDW API (m9d7-ebf2.json).
    - Printed API response to the console.
    - Handled errors with a Toast message.
    - Updated AndroidManifest.xml to include internet permission.
    - Added code comments

--- app/src/main/java/com/nathan/taxibedrijf_on_the_road/MainActivity.kt
+++ app/src/main/java/com/nathan/taxibedrijf_on_the_road/MainActivity.kt
@@ -3,8 +3,13 @@ package com.nathan.taxibedrijf_on_the_road
 import android.os.Bundle
 import android.widget.Button
 import android.widget.EditText
+import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
+import com.android.volley.Request
+import com.android.volley.RequestQueue
+import com.android.volley.toolbox.StringRequest
+import com.android.volley.toolbox.Volley
 
 class MainActivity : AppCompatActivity() {
 
@@ -30,5 +35,25 @@ class MainActivity : AppCompatActivity() {
                 .replace(R.id.fmResultaat, fragment)
                 .commit()
         }
+
+        //URL die gaat naar de data in een API, in json format.
+        val url = "https://opendata.rdw.nl/resource/m9d7-ebf2.json"
+        //Aanmaken van een wachtrij om de verzoeken in te zetten. Dit komt uit de library Volley.
+        //'this' is de instantie van MainActivity die momenteel runt, daarvan is de context van de applicatie nodig.
+        //Dit zorgt ervoor dat de RequestQueue gekoppeld is aan de gehele applicatie, niet alleen de activity.
+        val queue : RequestQueue = Volley.newRequestQueue(this.applicationContext)
+        //Vervolgens is er een StringRequest variabele aangemaakt, die een GET functie uitvoert. Dit betekent dat er data wordt opgehaald. Ook de API url is nodig.
+        val request = StringRequest(Request.Method.GET, url,
+            //Dan voeren we een print in de console uit als we response krijgen.
+            {response->
+                println(response.toString())
+            },
+            //Zo niet, dat geven we een error.
+            {error->
+                //Dit doen we door Toast te gebruiken, over de gehele applicatie, met als text "Geen Reactie!" en de duur is lang. .show() laat de Toast gelijk zien.
+                Toast.makeText(this.applicationContext, "Geen reactie!", Toast.LENGTH_LONG).show()
+            })
+        // Vervolgens moeten we nog wel de GET request in de RequestQueue zetten.
+        queue.add(request)
     }
 }
